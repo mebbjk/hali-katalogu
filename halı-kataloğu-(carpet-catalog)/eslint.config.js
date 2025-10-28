726bfbb5eed7c2563a4bf4e0d20a4ec943978307
Meb Bjk <32273221+mebbjk@users.noreply.github.com>
2025-10-28 03:23:38 +0300

    Update eslint.config.js

--- halı-kataloğu-(carpet-catalog)/eslint.config.js
+++ halı-kataloğu-(carpet-catalog)/eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintReact from "@eslint-react/eslint-plugin";
 import pluginReactRefresh from "eslint-plugin-react-refresh";
+import pluginReactHooks from "eslint-plugin-react-hooks";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -17,8 +18,10 @@ export default [
     files: ["**/*.{ts,tsx}"],
     plugins: {
       'react-refresh': pluginReactRefresh,
+      'react-hooks': pluginReactHooks,
     },
     rules: {
+      ...pluginReactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       '@typescript-eslint/no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],
       '@typescript-eslint/no-explicit-any': 'warn',
